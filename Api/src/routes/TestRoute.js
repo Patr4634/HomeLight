4b58a665495f5e408b39e2602fbce1af8a9864ec
Koldborg <akoldborg@gmail.com>
2022-04-04 13:57:59 +0200

    A bit of progress

---
+++ Api/src/routes/TestRoute.js
@@ -0,0 +1,13 @@
+//exports.Get = function(app) {
+//    const controller = require('../controllers/TestController');
+//    app.route('/T')
+//        .post(controller.Y);
+//}
+
+//export function Y(app) {
+//    const controller = require('../controllers/TestController');
+//    app.post('/Y', [
+//        controller.T,
+//        controller.Y
+//    ]);
+//}
